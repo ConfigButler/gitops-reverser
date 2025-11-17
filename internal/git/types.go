b25571f9ae24183cec3a53db81328c32e057219b
Simon Koudijs <simonkoudijs@gmail.com>
2025-11-17 18:35:26 +0000

    fix: Let's get our abstraction back into git :-)

--- internal/git/types.go
+++ internal/git/types.go
@@ -26,6 +26,39 @@ import (
 	"github.com/ConfigButler/gitops-reverser/internal/types"
 )
 
+// CommitFile represents a single file to be committed.
+type CommitFile struct {
+	Path    string
+	Content []byte
+}
+
+// BranchInfo contains information about a Git branch.
+type BranchInfo struct {
+	ShortName string // e.g., "main"
+	Sha       string // commit hash, normally the tip of the default branch. But will be empty ("") for an unborn branch that is going to be orphaned branch (if the default branch does not exist)
+	Unborn    bool   // Is true for branches that don't have commits yet: only HEAD is configured to it
+}
+
+// RepoInfo represents high-level repository information.
+type RepoInfo struct {
+	DefaultBranch     *BranchInfo
+	RemoteBranchCount int
+}
+
+// PullReport provides detailed pull operation results.
+type PullReport struct {
+	ExistsOnRemote  bool // Branch exists on remote
+	HEAD            BranchInfo
+	IncomingChanges bool // SHA changed, requiring resource-level reconcile
+}
+
+// WriteEventsResult provides detailed writeEvents operation results.
+type WriteEventsResult struct {
+	CommitsCreated int           // Number of succesfuly pushed commits (0 if no changes)
+	ConflictPulls  []*PullReport // List of PullReports: one for each conflict resolution attempt
+	Failures       int           // Number of failures while attempting to push commits (0 in ideal situation)
+}
+
 // BranchKey uniquely identifies a (GitRepoConfig, Branch) combination.
 // This is the unit of worker ownership to prevent merge conflicts.
 // Multiple GitDestinations can share the same BranchKey (same repo+branch)
